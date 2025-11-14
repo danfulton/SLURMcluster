ccd6cafd7a78b14dc2f4509254777be94e9d88c4
Dan Fulton <daniel.fulton@microsoft.com>
2025-11-14 21:18:28 +0000

    minor changes to make compute.sh idempotent

--- offline/compute.sh
+++ offline/compute.sh
@@ -13,17 +13,17 @@ sudo systemctl start munge
 sudo systemctl status munge
 sudo chown -R munge:munge /var/run/munge
 
-sudo addgroup --system --gid 986 slurm
-sudo adduser  --system --uid 992  --gid 986  --disabled-login --disabled-password --no-create-home --gecos "" --shell /usr/sbin/nologin slurm
+getent group slurm >/dev/null || sudo addgroup --system --gid 986 slurm
+id -u slurm >/dev/null 2>&1 || sudo adduser  --system --uid 992  --gid 986  --disabled-login --disabled-password --no-create-home --gecos "" --shell /usr/sbin/nologin slurm
 
-sudo mkdir /var/log/slurm
+sudo mkdir -p /var/log/slurm
 sudo chown slurm:slurm /var/log/slurm
 sudo mkdir -p /var/spool/slurm
 sudo chown slurm:slurm /var/spool/slurm
 
 sudo touch /var/log/slurmd.log
 sudo chown slurm:slurm /var/log/slurmd.log
-sudo mkdir /var/spool/slurmd
+sudo mkdir -p /var/spool/slurmd
 sudo chown slurm:slurm /var/spool/slurmd
 
 #sudo unlink /etc/profile.d/99_slurm_path.sh
@@ -43,7 +43,7 @@ echo 'export PATH=/share/sched/slurm/23.11.5/bin:/share/sched/slurm/23.11.5/sbin
 sudo ln -sf /share/sched/slurm/etc/slurm_path.sh /etc/profile.d/99_slurm_path.sh
 
 sudo systemctl enable slurmd
-sudo systemctl start slurmd
+sudo systemctl restart slurmd
 
 sudo cp -fv /usr/share/enroot/hooks.d/50-slurm-pmi.sh /usr/share/enroot/hooks.d/50-slurm-pytorch.sh /etc/enroot/hooks.d
 sudo sed -i '/set -eu/a export PATH=/share/sched/slurm/23.11.5/bin:$PATH' /etc/enroot/hooks.d/50-slurm-pytorch.sh
