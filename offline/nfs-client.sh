a2905e9f5238dfa0a67338f748c231a58c600c67
Dan Fulton <daniel.fulton@microsoft.com>
2025-11-14 21:16:52 +0000

    update NFS headnode and add mount to /etc/fstab to persist reboot

--- offline/nfs-client.sh
+++ offline/nfs-client.sh
@@ -2,8 +2,10 @@
 
 mount="/share"
 #plae the IP address of the headnode here
-server="10.7.0.10"
+server="180.9.20.214"
 
 #sudo apt install nfs-common -y
-sudo mkdir $mount
-sudo mount $server:$mount $mount
+sudo mkdir -p $mount
+grep -c "/share nfs4" /etc/fstab ||  sudo bash -c "echo \"${server}:/share /share nfs4 defaults,nofail 0 2\" >> /etc/fstab"
+sudo systemctl daemon-reload
+sudo mount $mount
