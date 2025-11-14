b521ff01d5bc257873b0f4eae9fa5d9a693beb2f
Dan Fulton <daniel.fulton@microsoft.com>
2025-11-14 21:17:24 +0000

    persist mount of worker nvme after reboot

--- offline/nvme.sh
+++ offline/nvme.sh
@@ -35,8 +35,11 @@ echo "RAID pair 1: ${nvmes[1]} ${nvmes[2]}"
 echo "RAID pair 2: ${nvmes[3]} ${nvmes[4]}"
 
 
-sudo mdadm --create /dev/md110 --level 0 --raid-devices 2 /dev/${nvmes[1]} /dev/${nvmes[2]}
-sudo mkfs.xfs /dev/md110
+grep -c md110 /proc/mdstat || sudo mdadm --create /dev/md110 --level 0 --raid-devices 2 /dev/${nvmes[1]} /dev/${nvmes[2]}
+grep -c resource_nvme /proc/mounts && sudo umount /mnt/resource_nvme
+sudo mkfs.xfs -f -L resourcenvme /dev/md110
 sudo mkdir /mnt/resource_nvme
-sudo mount /dev/md110 /mnt/resource_nvme
 sudo chmod 1777 /mnt/resource_nvme
+grep -c resource_nvme /etc/fstab ||  sudo bash -c 'echo "LABEL=resourcenvme /mnt/resource_nvme xfs defaults,nofail 0 2" >> /etc/fstab'
+sudo systemctl daemon-reload
+sudo mount /dev/md110 /mnt/resource_nvme
