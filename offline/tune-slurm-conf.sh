483ed2f05e807711eda0cc7763b2cf8b06feea6b
Dan Fulton <danielfulton@microsoft.com>
2025-11-19 04:47:58 +0000

    Auto set AccountingStorageHost in slurm.conf

--- offline/tune-slurm-conf.sh
+++ offline/tune-slurm-conf.sh
@@ -5,6 +5,7 @@ input_file="$1"
 node_list=$(grep -v '^\s*#' "$input_file" | tr -d '\r' | paste -sd,)
 
 sed -i "s/^SlurmctldHost=.*/SlurmctldHost=$HOSTNAME/" slurm.conf
+sed -i "s/^AccountingStorageHost=.*/AccountingStorageHost=$HOSTNAME/" slurm.conf
 
 echo "NodeName=${node_list} CPUs=144 Boards=1 SocketsPerBoard=2 CoresPerSocket=72 ThreadsPerCore=1 RealMemory=1635214 State=UNKNOWN Gres=gpu:4" >> slurm.conf
 echo "PartitionName=debug Nodes=ALL Default=YES MaxTime=INFINITE State=UP" >> slurm.conf
