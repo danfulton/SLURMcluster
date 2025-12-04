541b9c6a3738298a955fb554980f6f7353204d50
Daniel Fulton <daniel.fulton@microsoft.com>
2025-12-04 00:04:39 +0000

    Updated just about every file. OO_share.sh is not safe to run multiple times or after nvme.sh which has a fixed typo. Add 01_download_install_pkgs.sh to run the download scripts (updated to be safe to run multiple times) and sources then install / compile everything for a slurm cluster. Add 05_slurmdbd.sh to setup slurmdb in mariadb with a random password. Removed execute permissions on 03_lustre_alternative.sh. 03_nfs-server updated package install location. 06_controller updated to not install packages as that is handled in 01_download_install_pkgs.sh now. Also setup /etc/slurm as a symlink to /share/sched/slurm/etc. 07_compute pushes the fix-user-namespace.sh and runs it. The compute.sh has been updated to handle the new install package locations and removed some other lines that ar enot needed. download_debs.sh and download_sources.sh updated to be albe to be run multiple times and use apt download or curl. Remove deprecated line from cgroups.conf. install_packages-offline.sh can likely be deleted now but it no longer will install packages as dpkg is commented out. Updated 04_nfs-client to use the same hostsfile.txt

---
+++ offline/01_download_install_pkgs.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+
+
+module load mpi/hpcx
+
+export SCHEDROOT=/share/sched
+if [[ -e  /etc/slurm ]]; then
+    if [[ ! -L /etc/slurm ]]; then
+        sudo mv /etc/slurm /etc/slurm.bak
+        mkdir -p  ${SCHEDROOT}/slurm/etc
+        sudo ln -sT ${SCHEDROOT}/slurm/etc /etc/slurm
+    fi
+else
+    mkdir -p ${SCHEDROOT}/slurm/etc
+    sudo ln -sTf ${SCHEDROOT}/slurm/etc /etc/slurm
+fi
+
+./offline-debs/download_debs.sh
+./sources/download_sources.sh
+
+sudo apt -y install /share/offline-debs/mariadb/*.deb
+sudo apt -y install /share/offline-debs/misc/*.deb
+sudo apt -y install /share/offline-debs/slurm/*.deb  /share/offline-debs/slurm/server/*.deb /share/offline-debs/slurm/build-slurm/*.deb
+
+
+mkdir -p /share/build
+cd /share/build
+tar -xf  /share/sources/v0.20.0.tar.gz
+tar -xf  /share/sources/slurm-25.11.0.tar.bz2
+cd slurm-25.11.0
+sudo mk-build-deps -i debian/control
+
+ARCH=$(uname -m) 
+sed -i "s|dh_auto_configure -- --sysconfdir=/etc/slurm --disable-debug --with-mysql_config --with-slurmrestd --with-pmix --enable-pam --with-pam_dir=/usr/lib/\$(DEB_HOST_MULTIARCH)/security --with-systemdsystemunitdir=/lib/systemd/system/ SUCMD=/bin/su SLEEP_CMD=/bin/sleep$|dh_auto_configure -- --sysconfdir=/etc/slurm --disable-debug --with-mysql_config --with-slurmrestd --with-pmix=/usr/lib/${ARCH}-linux-gnu/pmix2 --enable-pam --with-pam_dir=/usr/lib/\$(DEB_HOST_MULTIARCH)/security --with-systemdsystemunitdir=/lib/systemd/system/ SUCMD=/bin/su SLEEP_CMD=/bin/sleep  --disable-x11 --with-json --with-jwt --with-http-parser --with-yaml --with-hdf5=yes --with-lz4 --with-hwloc --with-nvml --with-lua --with-munge --with-libcurl|" debian/rules
+debuild -b -uc -us
+mkdir -p /share/offline-debs/slurm-local-build/
+cp ../slurm*.deb /share/offline-debs/slurm-local-build/
+cd /share/offline-debs/slurm-local-build/
+rm *sview*.deb
+mkdir -p server
+mv slurm-smd-slurmctld*.deb  slurm-smd-slurmdbd*.deb  slurm-smd-slurmrestd*.deb server/
+
+sudo apt -y install /share/offline-debs/slurm-local-build/*.deb /share/offline-debs/slurm-local-build/server/*.deb
+
+rm -rf /share/build/slurm-25.11.0
+
+cd /share/build/pyxis-0.20.0
+make orig
+make deb
+cp ../nvslurm-plugin-pyxis*.deb /share/offline-debs/slurm/
+sudo apt -y install /share/offline-debs/slurm/nvslurm-plugin-pyxis*.deb
+
+
+cd "$(dirname "$(readlink -f "$0")")"
+echo $(pwd)
+sudo mkdir -p /var/log/slurm
+sudo chown -R slurm:slurm /var/log/slurm
+sudo mkdir -p /var/spool/slurm
+sudo chown -R slurm:slurm /var/spool/slurm
+sudo mkdir -p $SCHEDROOT/slurm/etc/slurmctld_state
+sudo chown -R slurm:slurm $SCHEDROOT/slurm/etc/slurmctld_state
+sudo chmod -R 755 $SCHEDROOT/slurm/etc/slurmctld_state
+sudo mkdir -p $SCHEDROOT/slurm/etc/plugstack.conf.d
+echo 'include /share/sched/slurm/etc/plugstack.conf.d/*' | sudo tee $SCHEDROOT/slurm/etc/plugstack.conf
+sudo chown slurm:slurm $SCHEDROOT/slurm/etc/plugstack.conf
+echo "required /usr/lib/${ARCH}-linux-gnu/slurm/spank_pyxis.so" | sudo tee $SCHEDROOT/slurm/etc/plugstack.conf.d/pyxis.conf
+sudo chown slurm:slurm $SCHEDROOT/slurm/etc/plugstack.conf.d/pyxis.conf
+
+~/SLURMcluster/offline/tune-gres-conf.sh hostfile.txt
+~/SLURMcluster/offline/tune-slurm-conf.sh hostfile.txt
+
+sudo chown -R slurm:slurm $SCHEDROOT/slurm
+
