2e80c63218f7a45665e410cd2b2249acf7af16fe
Daniel Fulton <danielfulton@microsoft.com>
2025-12-10 06:13:58 +0000

    Change tune-gres-conf.sh to remove the gres.conf file if it exists before creating it again.

--- offline/tune-gres-conf.sh
+++ offline/tune-gres-conf.sh
@@ -2,6 +2,9 @@
 
 input_file="$1"
 
+if [[ -e gres.conf ]]; then
+        rm gres.conf
+fi
 for i in $(cat ${input_file}); do
 	echo "Nodename=$i Name=gpu Count=4 File=/dev/nvidia[0-3]" >> gres.conf
 done
