5ed81331182d249e4127bdb46eb8bf154bef51a9
Daniel Fulton <danielfulton@microsoft.com>
2025-12-12 20:26:35 +0000

    Disable apparmor

--- offline/fix-user-namespace.sh
+++ offline/fix-user-namespace.sh
@@ -2,10 +2,13 @@
 #
 
 
-sudo cp /share/apparmor.profile /etc/apparmor.d/enroot
+#sudo cp /share/apparmor.profile /etc/apparmor.d/enroot
+sudo rm /etc/apparmor.d/enroot /etc/apparmor.d/usr.bin.enroot-nsenter
 sudo aa-complain /usr/bin/enroot-nsenter
 sudo aa-complain /etc/apparmor.d/*
 sudo sysctl -w kernel.apparmor_restrict_unprivileged_userns=0
 echo 'kernel.apparmor_restrict_unprivileged_userns=0' | sudo tee -a /etc/sysctl.d/99-enroot.conf
-sudo apparmor_parser -R /etc/apparmor.d/enroot
+#sudo apparmor_parser -R /etc/apparmor.d/enroot
+sudo systemctl stop apparmor
+sudo systemctl disable apparmor
 
