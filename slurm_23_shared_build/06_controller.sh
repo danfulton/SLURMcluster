ddf45e0ffef431d9e0d0b57067fffae17b6a138d
Daniel Fulton <danielfulton@microsoft.com>
2025-12-15 08:13:03 +0000

    Forgot to uncommit some items. - adam

--- slurm_23_shared_build/06_controller.sh
+++ slurm_23_shared_build/06_controller.sh
@@ -58,8 +58,8 @@ sudo chown slurm:slurm /var/spool/slurmctld
 sudo cp -fv /usr/share/enroot/hooks.d/50-slurm-pmi.sh /usr/share/enroot/hooks.d/50-slurm-pytorch.sh /etc/enroot/hooks.d
 
 
+sudo unlink /etc/profile.d/99_slurm_path.sh
 echo 'export PATH=/share/sched/slurm/23.11.5/bin:/share/sched/slurm/23.11.5/sbin:$PATH' | sudo tee /share/sched/slurm/etc/slurm_path.sh
-unlink /etc/profile.d/99_slurm_path.sh
 sudo ln -sf /share/sched/slurm/etc/slurm_path.sh /etc/profile.d/99_slurm_path.sh
 
 sudo systemctl enable slurmdbd 
--- slurm_23_shared_build/07_compute.sh
+++ slurm_23_shared_build/07_compute.sh
@@ -2,8 +2,8 @@
 
 file=$1
 
-parallel-scp -p 256 -t 60 -h ${file} $PWD/compute.sh ~/compute.sh
-parallel-scp -p 256 -t 60 -h ${file} $PWD/fix-user-namespace.sh ~/fix-user-namespace.sh
+parallel-scp -p 256 -t 10 -h ${file} $PWD/compute.sh ~/compute.sh
+parallel-scp -p 256 -t 10 -h ${file} $PWD/fix-user-namespace.sh ~/fix-user-namespace.sh
 #parallel-scp -h ${file} $PWD/apparmor.profile ~/apparmor.profile
-parallel-ssh -p 256 -t 600 -i -h ${file} ~/compute.sh
-parallel-ssh -p 256 -t 600 -i -h ${file} ~/fix-user-namespace.sh
+parallel-ssh -p 256 -t 60 -i -h ${file} ~/compute.sh
+parallel-ssh -p 256 -t 60 -i -h ${file} ~/fix-user-namespace.sh
