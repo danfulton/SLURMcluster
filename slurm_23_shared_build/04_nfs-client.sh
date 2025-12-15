7c8fea8377017ccd14bfc7b32f86ef365dcd677e
Daniel Fulton <daniel.fulton@microsoft.com>
2025-12-15 18:43:01 +0000

    Final updated for Dec. reverting  04_nfs-client.sh to default blank host. 07_compute.sh fixed permissions and other changes. nfs-client.sh and slurmdbd.conf revert to blank versions for pass and host fields.

--- slurm_23_shared_build/04_nfs-client.sh
+++ slurm_23_shared_build/04_nfs-client.sh
@@ -2,5 +2,7 @@
 
 sed -i "s/^NFSSERVER=.*/NFSSERVER=$(hostname -s)/" $(dirname ${BASH_SOURCE[0]})/nfs-client.sh
 
-parallel-scp -h hostfile.txt $PWD/nfs-client.sh ~/nfs-client.sh
-parallel-ssh -i -h hostfile.txt ~/nfs-client.sh
+parallel-scp -t 10 -p 256 -h  hostfile.txt /etc/hosts /tmp/
+parallel-ssh -i -t 10 -p 256 -h  hostfile.txt "sudo cp /tmp/hosts /etc/hosts"
+parallel-scp -t 10 -p 256 -h hostfile.txt $PWD/nfs-client.sh ~/nfs-client.sh
+parallel-ssh -i  -t 10 -p 256 -h hostfile.txt ~/nfs-client.sh
--- slurm_23_shared_build/nfs-client.sh
+++ slurm_23_shared_build/nfs-client.sh
@@ -2,7 +2,7 @@
 
 mount="/share"
 #place the IP address of the headnode here
-NFSSERVER=ATL210108150121
+NFSSERVER=""
 
 if [ -z "$NFSSERVER" ]; then
 	echo "NFS server address is not set!"
